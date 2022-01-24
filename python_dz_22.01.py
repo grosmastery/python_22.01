a6517f8c71ccad0c8aedf81b989ef74c238d036c
grosmastery <grosmastery@gmail.com>
2022-01-24 15:23:51 +0200

    'comment'

---
+++ python_dz_22.01.py
@@ -0,0 +1,17 @@
+int_number = input('Введите значение: ')
+int_number = int(int_number)
+print('Вы ввели:', int_number)
+print('Pезультат:', int_number * 2)
+
+#################################################
+
+float_number = input('Введите значение: ')
+float_number = float(float_number)
+print('Вы ввели:', float_number)
+print('Результат:', float_number + float_number)
+
+#################################################
+
+double_string = input ('Введите символы: ')
+print('Вы ввели:', double_string)
+print('Результат:', double_string, double_string)
